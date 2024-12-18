e3cf9cf1efbe97b9be9b1eab8383bb6830f429fb
danylo8 <ozai.avatar@gmail.com>
2024-12-18 11:42:37 -0500

    Up to the test cases. They aren't working properly.

--- Sign.java
+++ Sign.java
@@ -1,2 +1,30 @@
 public class Sign {
+    private String message;
+    private int width;
+
+    public Sign(String m, int w) {
+        message = m;
+        width = w;
+    }
+
+    public int numberOfLines() {
+        int lines = message.length();
+        if (message.length() % width == 0) return lines;
+        return lines + 1;
+
+    }
+
+    public String getLines() {
+        String s = "";
+        int i = 0;
+        while (i < message.length()) ;
+        {
+            s += message.substring(i, i + 1);
+            i++;
+            if (i % width == 0 && i != message.length()) s += ";";
+        }
+        return s;
+
+
+    }
 }
